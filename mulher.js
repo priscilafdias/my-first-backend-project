1839ded32bd6d84816aa3a98c26415dc1f7068e6
priscilafdias <priscilafdias@gmail.com>
2023-05-25 12:15:58 +0100

    adicionando primeira versao do meu projeto

---
+++ mulher.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+function mostraMulher(request, response) {
+    response.json({
+        nome: 'Priscila Dias',
+         })
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta", porta)
+}
+
+app.use(router.get('/mulher', mostraMulher))
+app.listen(porta, mostraPorta)
